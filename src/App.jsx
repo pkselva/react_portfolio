1eefae8f2bde690d25bf072f419075efcad0f08e
pkselva <karthickselvan07@gmail.com>
2025-10-11 15:18:30 +0530

    Layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/skills" element={<SignIn />} />
-        <Route path="/project" element={<SignUp />} />
-        <Route path="/contact" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+    
+    </>
   )
 }
 
--- src/Pages/Layout.jsx
+++ src/Pages/Layout.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import NavgationBar from '../Components/NavgationBar'
+import HeaderSection from '../Components/HeaderSection'
+import About from '../Components/About'
+import Skills from '../Components/Skills'
+import Projects from '../Components/Projects'
+import Contact from '../Components/Contact'
 
 function Layout() {
   return (
-    <div>Layout</div>
+    <>
+      <NavgationBar />
+      <HeaderSection />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </>
   )
 }
 
