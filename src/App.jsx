a0ff25d7c259a721668babd30e4047b71252628f
Gowshik S <gowshik.webdev@gmail.com>
2025-10-11 16:00:45 +0530

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Layout from './Pages/Layout'
 
 function App() {
   return (
     <>
-    
+      <Layout/>
     </>
   )
 }
