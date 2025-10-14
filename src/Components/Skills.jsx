feb3df228f2b10ea81171d7647472477edb53bd2
pkselva <karthickselvan07@gmail.com>
2025-10-15 00:33:56 +0530

    Skills component styles added

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -20,13 +20,11 @@ function Skills() {
         <div className='grid grid-cols-2 md:grid-cols-4 xl:grid-cols-8 gap-5 items-center justify-center'>
           {skillsCard.map((val, idx) => {
             return (
-              <div key={idx} className='px-8 cursor-pointer w-full sm:w-[150px] flex flex-col justify-center items-center py-[25px] 
-                         border border-[#ffffff1a] bg-[#1f1f1f] rounded-[14px] 
-                         hover:border-[#c9f31d] hover:text-[#c9f31d] transition'>
-                <div className='w-[30px] sm:w-[60px]'>
+              <div key={idx} className='px-8 cursor-pointer w-full sm:w-[150px] flex flex-col justify-center items-center py-[25px] border border-[#ffffff1a] bg-[#1f1f1f] rounded-[14px] hover:border-[#c9f31d] text-white hover:text-[#c9f31d] transform hover:scale-102 hover:-translate-y-1  transition duration-300 ease-in-out'>
+                <div className='w-[40px] sm:w-[60px]'>
                   <img src={val.img} alt={val.label} />
                 </div>
-                <div className='leading-[28px] text-[20px] mt-[15px] text-white'>{val.label}</div>
+                <div className='leading-[28px] text-[20px] mt-[15px]'>{val.label}</div>
               </div>
             )
           })}
