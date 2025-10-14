b355b29637b3a06bf45b1dbd00628b6bb4548e31
pkselva <karthickselvan07@gmail.com>
2025-10-15 00:23:17 +0530

    Navbar revamped for mobile responsive

--- src/Components/NavgationBar.jsx
+++ src/Components/NavgationBar.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from "react";
 
-function NavgationBar() {
+function NavigationBar() {
+  const [isOpen, setIsOpen] = useState(false);
 
   const navList = [
     { link: "#home", value: "Home" },
@@ -9,32 +9,52 @@ function NavgationBar() {
     { link: "#skills", value: "Skills" },
     { link: "#projects", value: "Projects" },
     { link: "#contact", value: "Contact" },
-  ]
+  ];
 
   return (
-    <div className='bg-[#1f1f1f] flex items-center px-[15px]'>
-      <div className='w-full text-left'>
-        <img src="/logo.png" alt="logo" width={175} height={40}/>
+    <div className="bg-[#1f1f1f] flex items-center justify-between px-4 py-3">
+
+      <div className="flex-shrink-0">
+        <img src="/logo.png" alt="logo" width={150} height={40} />
       </div>
-      <div className='w-full flex justify-center'>
-        {navList.map((val, idx) => {
-          return (
-            <div key={idx} className='px-5 py-[25px] leading-[30px] text-white font-[500]'>
-              <a href={val.link}>
-                {val.value}
-              </a>
-            </div>
-          )
-        }
-        )}
+
+      <div className="hidden md:flex justify-end flex-1">
+        {navList.map((val, idx) => (
+          <a
+            key={idx}
+            href={val.link}
+            className="px-5 py-2 text-white font-medium hover:text-[#c9f31d] transition"
+          >
+            {val.value}
+          </a>
+        ))}
       </div>
-      <div className='w-full flex justify-end'>
-        <button className='bg-[#c9f31d] rounded-full w-[50px] h-[50px] flex justify-center items-center cursor-pointer'>
-          <img src="./sidebar-tottler.svg" alt="sidebar" />
+
+      <div className="md:hidden">
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="bg-[#c9f31d] rounded-full w-[45px] h-[45px] flex justify-center items-center"
+        >
+          <img src="/sidebar-tottler.svg" alt="sidebar" />
         </button>
       </div>
+
+      {isOpen && (
+        <div className="absolute top-[70px] left-0 w-full bg-[#1f1f1f] flex flex-col items-center md:hidden z-50">
+          {navList.map((val, idx) => (
+            <a
+              key={idx}
+              href={val.link}
+              className="py-3 w-full text-center text-white hover:bg-[#c9f31d] hover:text-black transition"
+              onClick={() => setIsOpen(false)}
+            >
+              {val.value}
+            </a>
+          ))}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default NavgationBar
+export default NavigationBar;
