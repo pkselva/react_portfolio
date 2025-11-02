1c3a8b8ef165d477753afbe263576f07c5456d0a
Karthick Selvan <karthickselvan07@gmail.com>
2025-11-03 01:10:01 +0530

    test api added

--- api/server.js
+++ api/server.js
@@ -13,6 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contact", contactRoutes);
+app.get("/", (_, res) => {
+  res.send("API is running...");
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
